7ef4ec1f9581f630c9ad7a86d8980b6b98173c69
Richard Si <sichard26@gmail.com>
2025-07-14 18:30:59 -0400

    Separate plugin from package root

--- setup.py
+++ setup.py
@@ -7,16 +7,13 @@ from setuptools.command.build_py import build_py
 class build_py_with_pth_file(build_py):
     """Include the .pth file for this project, in the generated wheel."""
 
-    pth_file = "pytest_socket.pth"
+    pth_file = Path("src", "pytest_socket.pth")
 
     def run(self):
         super().run()
 
-        self.copy_file(
-            self.pth_file,
-            str(Path(self.build_lib, self.pth_file)),
-            preserve_mode=0,
-        )
+        target = Path(self.build_lib, self.pth_file.name)
+        self.copy_file(self.pth_file, target, preserve_mode=0)
 
 
 setup(cmdclass={"build_py": build_py_with_pth_file})
